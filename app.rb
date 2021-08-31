cdd96b82be881e0014f4bdee0f6a8a8b28786b3c
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-08-31 14:53:16 -0400

    commit tree 7b7d4ce244132c916a34343394cabfd7d7c5bfb4

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
-
+require 'pry'
+require_relative "models/piglatinizer.rb"
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        pl = PigLatinizer.new
+        @piglatin = pl.piglatinize(params[:user_phrase])
+        erb :piglatinize
+        # binding.pry
+    end
+
 end
